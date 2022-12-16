3c0781094d8b3e009377c365c30df6c078bfc6d2
Christopher Narayanan <93202854+Queentaker@users.noreply.github.com>
2022-12-16 23:10:32 +0100

    creating a temporary grid factory

--- src/grid/GridCell.java
+++ src/grid/GridCell.java
@@ -1,27 +1,35 @@
 package grid;
 
 public class GridCell {
+
     private boolean isOccupied;
-    private int playerId;
+    private String playerId;
+    //todo flyweight
+
 
 
     public GridCell(){
         isOccupied=false;
     }
+    public GridCell(String playerId){
+        this.playerId=playerId;
+        isOccupied=true;
+    }
 
     public boolean isOccupied() {
         return isOccupied;
     }
-    public int getPlayerId(){
+    public String  getPlayerId(){
+        assert playerId!=null;
         return playerId;
     }
-    public void place(int playerId){
+    public void place(String playerId){
         isOccupied=true;
         this.playerId=playerId;
     }
-    public void remove (int playerId){
+    public void remove (String playerId){
         isOccupied=false;
-        this.playerId=0;
+
     }
 
 }
--- src/grid/GridCellFactory.java
+++ src/grid/GridCellFactory.java
@@ -1,5 +1,41 @@
 package grid;
 
+import java.util.HashMap;
+
 public class GridCellFactory {
+    private HashMap<String,GridCell> occupiedGridCells= new HashMap<>();
+    private GridCell emptyCell;
+
+    public GridCell getEmptyGridCell() {
+        if (emptyCell==null){
+            emptyCell= new GridCell();
+        }
+        return emptyCell;
+    }
+
+    public GridCell getGridCell(String id){
+        if (!occupiedGridCells.containsKey(id)){
+            occupiedGridCells.put(id, new GridCell(id));
+        }
+        return occupiedGridCells.get(id);
+    }
+
     //todo
+        /*
+
+
+public class FlyweightFactory {
+    private HashMap<String, Flyweight> flyweights = new HashMap<>();
+
+    public Flyweight getFlyweight(String intrinsicState) {
+        if (!flyweights.containsKey(intrinsicState)) {
+            flyweights.put(intrinsicState, new Flyweight(intrinsicState));
+        }
+        return flyweights.get(intrinsicState);
+    }
+}
+
+
+
+     */
 }
