b77bf940ce34aee3f09759c8170cfd7b494bc8cb
Christopher Narayanan <93202854+Queentaker@users.noreply.github.com>
2022-12-21 23:42:27 +0100

    evolve big update

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,4 @@
 import GUI.Frame;
-import player.HumanPlayer;
-import player.Player;
-import player.PlayersSignature;
-import player.Signature;
-
-import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
--- src/grid/EvolveNextGen.java
+++ src/grid/EvolveNextGen.java
@@ -2,50 +2,92 @@ package grid;
 
 import player.PlayersSignature;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class EvolveNextGen {
     private Grid beforeEvolution;
     private int upperBoundary;
-    private final GridCellFactory gridCellFactory=GridCellFactory.getInstance();
-    public void evolve(Grid grid){
-        beforeEvolution= new Grid(grid);
-        upperBoundary= beforeEvolution.getGridSize();
+    private int lowerBoundary = 0;
+    private final GridCellFactory gridCellFactory = GridCellFactory.getInstance();
 
-        for (int i=0; i<upperBoundary;i++){
-            for (int j=0;j<upperBoundary;j++){
-                //does
+    public void evolve(Grid grid) {
+        beforeEvolution = new Grid(grid);
+        upperBoundary = beforeEvolution.getGridSize();
+
+        for (int x = 0; x < upperBoundary; x++) {
+            for (int y = 0; y < upperBoundary; y++) {
+                Map<PlayersSignature, NumNeighbors> aliveNeighborsMap = getAliveNeighborsMap(x, y);
+                if (!doesGridCellSurvive(aliveNeighborsMap)){
+                    grid.setGridCell(x,y,gridCellFactory.getEmptyGridCell());
+                }
+                else {
+                    grid.setGridCell(x,y,gridCellFactory.getGridCell(getSignatureAliveCell(aliveNeighborsMap)));
+                }
             }
         }
         //work needs to be done
     }
-    private NumNeighbors getAliveNeighbors(int x, int y){
-        Map<PlayersSignature,NumNeighbors> neighborCount=new HashMap<PlayersSignature,NumNeighbors>();
 
-        NumNeighbors counterAliveNeighbors=NumNeighbors.Zero;
-        //needs to be implemented correctly current version doesn't work
-        for (int i=-1 ; i<2 ; i++)
-            for (int j=-1 ; j<2 ; j++)
-                if (i !=0 && j != 0)
-                {
+    private Map<PlayersSignature, NumNeighbors> getAliveNeighborsMap(int x, int y) {
+        Map<PlayersSignature, NumNeighbors> neighborCount = new HashMap<PlayersSignature, NumNeighbors>();
+        for (int i = -1; i < 2; i++)
+            for (int j = -1; j < 2; j++)
+                if (i != 0 && j != 0) {
                     int row = x + i;
                     int colum = y + j;
 
-                    int lowerBoundary = 0;
-                    if (row >= lowerBoundary && colum >= lowerBoundary && row < upperBoundary && colum < upperBoundary)
-                    {
-                        GridCell gridCell=beforeEvolution.getGridCell(row,colum);
-                        //if (gridCell.isOccupied() && gridCell.getPlayerId()==playerId)
-                        //{
-                          //  counterAliveNeighbors++;
-                        //}
+                    if (doesCoordinateExist(row, colum)) {
+                        updateHashTable(row, colum, neighborCount);
                     }
                 }
-        return counterAliveNeighbors;
+        return neighborCount;
     }
-    private boolean doesGridCellSurvive(int x, int y){
-        NumNeighbors aliveNeighbors=getAliveNeighbors(x,y);
-        return aliveNeighbors==NumNeighbors.Two || aliveNeighbors==NumNeighbors.Three;
+
+    private boolean doesCoordinateExist(int row, int colum) {
+        return row >= lowerBoundary && colum >= lowerBoundary && row < upperBoundary && colum < upperBoundary;
+    }
+
+    private void updateHashTable(int row, int colum, Map<PlayersSignature, NumNeighbors> neighborCount) {
+        GridCell gridCell = beforeEvolution.getGridCell(row, colum);
+        if (gridCell.isOccupied()) {
+            PlayersSignature signature = gridCell.getPlayersSignature();
+            if (!neighborCount.containsKey(signature)) {
+                neighborCount.put(signature, NumNeighbors.One);
+            } else {
+                neighborCount.put(signature, NumNeighbors.getEnum(neighborCount.get(signature).value + 1));
+            }
+        }
+    }
+
+    private boolean doesGridCellSurvive(Map<PlayersSignature, NumNeighbors> numNeighborsMap) {
+        for (NumNeighbors numberOfNeighbors : numNeighborsMap.values()) {
+            if (requiredNeighborsToSurvive(numberOfNeighbors)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean requiredNeighborsToSurvive(NumNeighbors aliveNeighbors) {
+        return aliveNeighbors == NumNeighbors.Two || aliveNeighbors == NumNeighbors.Three;
+    }
+    private PlayersSignature getSignatureAliveCell(Map<PlayersSignature, NumNeighbors> numNeighborsMap){
+        List<PlayersSignature> survivingPlayersSignatures=new ArrayList<>();
+        for (Map.Entry<PlayersSignature, NumNeighbors> entry : numNeighborsMap.entrySet()){
+            PlayersSignature playersSignature=entry.getKey();
+            NumNeighbors numNeighbors=entry.getValue();
+            if (requiredNeighborsToSurvive(numNeighbors)){
+                survivingPlayersSignatures.add(playersSignature);
+            }
+        }
+        assert survivingPlayersSignatures.size()>0;
+        return randomValidPlayer(survivingPlayersSignatures);
+    }
+    //this needs to be done for the scenario that a player a has 2 neigbhoring and player 3 b to cell x
+    //randomly a playerSignature is going to be selected
+    private PlayersSignature randomValidPlayer(List<PlayersSignature> survivingPlayersSignatures){
+        int length=survivingPlayersSignatures.size();
+        Random rand= new Random();
+        return survivingPlayersSignatures.get(rand.nextInt(length));
     }
 }
