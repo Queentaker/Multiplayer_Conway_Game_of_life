d524f36565b4dd7ada67fa370003957587aa8a07
daniel <daniel.maksimovic01@gmail.com>
2022-12-27 18:11:02 +0100

    new RadioButton

---
+++ src/GUI/setUp/setUpElements/GridSetting/RadioButton.java
@@ -0,0 +1,18 @@
+package GUI.setUp.setUpElements.GridSetting;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class RadioButton extends JRadioButton implements ActionListener {
+
+    public RadioButton(String imageName){
+
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
