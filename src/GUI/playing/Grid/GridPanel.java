51a4bf2fe9523432db63c316cabb6a023d87aa68
ChlineSaurus <95419378+ChlineSaurus@users.noreply.github.com>
2022-12-27 16:51:27 +0100

    Git Problem

--- src/GUI/playing/Grid/GridPanel.java
+++ src/GUI/playing/Grid/GridPanel.java
@@ -8,12 +8,11 @@ import java.awt.*;
 public class GridPanel extends JPanel {
     public GridPanel(int length, int height) {
         this.setBackground(ColorScheme.BRIGHT_COLOR.getColor());
-        this.setLayout(new GridLayout(length, height));
-        for(int x = 0; x < length; x++) {
-            for(int y = 0; y < height; y++) {
+        this.setLayout(new GridLayout(height,length,0,0));
+        for(int x = 0; x < height; x++) {
+            for(int y = 0; y < length; y++) {
                 this.add(new GridButton(x,y));
             }
         }
     }
-
 }
--- src/GUI/playing/stats/StatesPanel.java
+++ src/GUI/playing/stats/StatesPanel.java
@@ -1,5 +1,6 @@
 package GUI.playing.stats;
 
+import GUI.Enums.ColorScheme;
 import GUI.Enums.FontScheme;
 
 import javax.swing.*;
@@ -9,10 +10,20 @@ public class StatesPanel extends JPanel {
     private final StatsProgressBar progressBar;
     private final StatsPlayerPanel player1Panel;
     private final StatsPlayerPanel player2Panel;
+    private final JLabel generation;
 
     public StatesPanel(Color player1Color, String player1Name, Color player2Color, String player2Name, int livingCells) {
         this.setOpaque(false);
         this.setLayout(new BorderLayout());
+
+        generation = new JLabel("Generation: 0");
+        generation.setOpaque(false);
+        generation.setHorizontalAlignment(JLabel.CENTER);
+        generation.setForeground(ColorScheme.DARK_COLOR.getColor());
+        generation.setFont(new Font(FontScheme.STANDARD_FONT.getName(), Font.BOLD, 24));
+        generation.setPreferredSize(new Dimension(200, 30));
+        this.add(generation, BorderLayout.NORTH);
+
         progressBar = new StatsProgressBar(player1Color, player2Color);
         this.add(progressBar, BorderLayout.CENTER);
 
@@ -22,7 +33,7 @@ public class StatesPanel extends JPanel {
         this.add(player2Panel, BorderLayout.EAST);
 
         this.setBackground(Color.WHITE);
-        this.setPreferredSize(new Dimension(400,75));
+        this.setPreferredSize(new Dimension(400,100));
     }
 
     public void update() {
